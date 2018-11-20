58809ee710f08669a205654f1b46381e0baa9157
Shemar Dacosta <dshemar6@gmail.com>
2018-11-20 15:31:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,5 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return [...array, element]
+  return [element]
 }
